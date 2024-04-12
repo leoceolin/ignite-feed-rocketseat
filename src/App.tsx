ac9f302ad50a37e97a1d27af6ddba058b3f51149
kobe-leonel <leonel.farias@kobe.io>
2024-04-12 10:59:58 -0300

    upload project and add global styles

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header'
+import './global.css'
+
+function App() {
+  return (
+    <p>
+      <Header />
+      ola mundo
+    </p>
+  )
+}
+
+export default App
